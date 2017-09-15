082782dbd30f2254f14ed840784b5ee248faab17
troytheboy <tbarn2@uw.edu>
2017-09-14 23:42:10 -0700

    moved bootstrap and fontawesome into src for building

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
+import './bootstrap.min.css'
+import './font-awesome.min.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
